a3b1d1e47945838acaa581c8c230f88ad2cad5d1
Marco <marco.salman@gmail.com>
2022-03-05 17:51:12 -0500

    adding set and fetch

--- express_server.js
+++ express_server.js
@@ -3,18 +3,31 @@ const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
-})
+});
 
-app.get("/urls.json", (req,res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>");
+});
+
+app.get("/set", (req,res) => {
+  const a = 1;
+  res.send(`a = ${a}`)
+})
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+ 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
