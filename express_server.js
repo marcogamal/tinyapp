69d5e8f28ccd2f708959f5f24c411f8e81b30195
Marco <marco.salman@gmail.com>
2022-03-06 10:41:02 -0500

    adding redirect

--- express_server.js
+++ express_server.js
@@ -1,22 +1,55 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+characters.length));
+ }
+ return result;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
+
 app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars)
 })
+
+app.post("/urls", (req,res) => {
+    const randomShort = generateRandomString();
+    urlDatabase[randomShort] = {
+      longURL: req.body.longURL,
+    };
+    return res.redirect(`/urls/${randomShort}`);
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -39,6 +72,20 @@ app.get("/set", (req,res) => {
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.post("/urls", (req,res) => {
+   console.log(req.body)
+   res.send("OK")
+ })
+
+ // requests to the endpoint "/u/:shortURL" will redirect to its longURL 
+ app.get("/u/:shortURL", (req, res) => {
+  if (urlDatabase[req.params.shortURL]) {
+    const fullURL = urlDatabase[req.params.shortURL].longURL;
+    return res.redirect(fullURL);
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
