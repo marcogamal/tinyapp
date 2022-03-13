3df505762c24b2747c95ffc8540fa66094204b2d
Marco <marco.salman@gmail.com>
2022-03-13 14:42:29 -0400

    adding cookie session

--- express_server.js
+++ express_server.js
@@ -4,10 +4,34 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
+const cookieSession = require("cookie-session");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["Marco123!@#"],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+
+//users
+const users = {
+  Asta: {
+    id: "Asta",
+    email: "asta@asta.com",
+    password: bcrypt.hashSync("asta1"),
+  },
+  kio: {
+    id: "kio",
+    email: "kio@kio.com",
+    password: bcrypt.hashSync("kio1"),
+  },
+};
 
 const urlDatabase = {
   b6UTxQ: {
@@ -29,20 +53,6 @@ function generateRandomString() {
   }
   return result;
 }
-
-//users
-const users = {
-  Asta: {
-    id: "Asta",
-    email: "asta@asta.com",
-    password: "asta1",
-  },
-  kio: {
-    id: "kio",
-    email: "kio@kio.com",
-    password: "kio1",
-  },
-};
 
 const findUserByEmail = (email) => {
   for (let userId in users) {
@@ -78,12 +88,16 @@ const urlsForUser = function (id) {
 
 // GET or POST
 app.get("/", (req, res) => {
-  res.redirect("/urls");
+  if (req.session.user_id) {
+    return res.redirect("/urls");
+  } else {
+    return res.redirect("/login");
+  }
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.session["user_id"]],
   };
   if (templateVars.user) {
     return res.render("urls_new", templateVars);
@@ -94,15 +108,16 @@ app.get("/urls/new", (req, res) => {
 
 // URL path with urls_index as the template
 app.get("/urls", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   const user = users[userId];
   if (!user) {
     return res
       .status(400)
       .send("Please <a href='/login'>login</a> to access the page");
   }
+  const urls = urlsForUser(user.id);
   const templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     urls: urlDatabase,
   };
   res.render("urls_index", templateVars);
@@ -120,7 +135,7 @@ app.post("/urls", (req, res) => {
 // URL/shortURL path with urls_show as the template
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
@@ -133,7 +148,7 @@ app.post("/urls/:shortURL", (req, res) => {
 
 //Delete button routing on server
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userId = req.cookies["user_id"];
+  const userId = req.session.user_id;
   if (!userId) {
     return res
       .status(400)
@@ -165,6 +180,7 @@ app.post("/register", (req, res) => {
   const email = req.body.email.trim();
   const password = req.body.password.trim();
   const user = findUserByEmail(email);
+  const hashedPassword = bcrypt.hashSync(password);
   if (user) {
     return res.status(400).send("Name is not allowed or User already exist");
   }
@@ -172,14 +188,14 @@ app.post("/register", (req, res) => {
     return res.status(400).send("Invalid credentials");
   }
   const userId = addNewUser(email, password, users);
-  res.cookie("user_id", userId);
+  req.session.user_id = userId;
   res.redirect("/urls");
 });
 
 //login using express cookies
 app.get("/login", (req, res) => {
   const templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.session.user_id],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
   };
@@ -189,6 +205,7 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email.trim();
   const password = req.body.password.trim();
+  const hashedPassword = bcrypt.hashSync(password);
   if (!email || !password) {
     return res
       .status(403)
@@ -196,19 +213,18 @@ app.post("/login", (req, res) => {
   }
   const user = findUserByEmail(email);
   console.log("User is", user);
-  if (!user || !bcrypt.compareSync(password, hashedPassword)) {
+  if (!user || !bcrypt.compareSync(password, user.password)) {
     return res
       .status(403)
       .send("Invalid credentials - please fix your username or password");
   }
-  res.cookie("user", { id: user.id, email: user.email });
-  res.cookie("user_id", user.id);
+  req.session.user_id = user.id;
   res.redirect("/urls");
 });
 
 //logout
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id", req.body.userId);
+  req.session.user_id = null;
   res.redirect("/urls");
 });
 
